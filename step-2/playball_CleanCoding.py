59992286d3fbd4431c948237e3080e9e7c95a23e
haveagood <haveagood@naver.com>
2019-12-01 16:23:17 +0900

    클린코딩 조건을 유지한채로 step-2의 첫번째 조건 구현 하였습니다.
    1. 각 함수는 15줄을 넘기지 않았습니다.
    
    2. 팀데이터를 입력받은채로 딕셔너리에 집어넣어 저장하였습니다.
    (별도의 파일을 만들어서 저장하지 않았고, while문 안에서 데이터를 저장한 채로 가지고 있습니다.)
    
    3. 팀 데이터를 출력하기 위해서 딕셔너리를 리스트로 변환하여 각 팀의 선수들을 출력하도록 하였습니다.
    
    ---------------------------------------------------------------
    
    4. 에러 처리에 대한 부분은 아직 미완성 입니다.

--- step-2/playball_CleanCoding.py
+++ step-2/playball_CleanCoding.py
@@ -1,27 +1,60 @@
-import random
-
 def main():
-    print("신나는 야구 시합")
-    print("1. 데이터 입력")
-    print("2. 데이터 출력")
-
-    print("메뉴 선택 ( 1 - 2 ) : ",end = " ")
-    menu_choice = input()
-    if int(menu_choice) == 1:
-        print("1번 메뉴")
-        input_data()
-    elif int(menu_choice) == 2:
-        print("2번 메뉴")
-        output_data()
-    else:
-        print("올바른 메뉴를 선택해 주세요!!")
+    while True:
+        print("신나는 야구 시합 \n1. 데이터 입력\n2. 데이터 출력\n메뉴 선택 ( 1 - 2 ) : ",end = "")
+        menu_choice = input()
+        if int(menu_choice) == 1:
+            team_1_name,team_1,team_2_name,team_2 = input_data()
+        elif int(menu_choice) == 2:
+            output_data(team_1_name,team_1,team_2_name,team_2)
+        # elif int(menu_choice) == 3:
+        #     playball(team_1_name,team_1,team_2_name,team_2)
+        #     break
+        else:
+            print("올바른 메뉴를 선택해 주세요!!")
 
 
 def input_data():
-    return
+    print("1번 메뉴")
+    team_1,team_2 = dict(),dict()
+    print("1 팀의 이름을 입력하세요> ",end = "")
+    team_1_name = input()
+    for i in range(1,10):
+        print("%d번 타자 정보 입력> " %(i),end = "")
+        name,hit = input().split(", ")
+        team_1[name] = float(hit)
 
-def output_data():
-    return
+    print("2 팀의 이름을 입력하세요> ",end = "")
+    team_2_name = input()
+    for j in range(1,10):
+        print("%d번 타자 정보 입력> " % (j),end = "")
+        name,hit = input().split(", ")
+        team_2[name] = float(hit)
+
+    print("팀 데이터 입력이 완료되었습니다.")
+
+    return team_1_name,team_1,team_2_name,team_2
+
+
+def output_data(team_1_name,team_1,team_2_name,team_2):
+    print("2번 메뉴")
+    print(" ---------------------------------------------- ")
+    print(team_1_name+"팀 정보")
+    change_dict(team_1)
+    print(" ---------------------------------------------- ")
+    print(team_2_name+"팀 정보")
+    change_dict(team_2)
+    print(" ---------------------------------------------- ")
+
+def change_dict(team_dict):
+    name_list = list(team_dict.keys())
+    hit_list = list(team_dict.values())
+    for i in range(len(name_list)):
+        print("%d번 %s, %0.3f" %((i+1),name_list[i],hit_list[i]))
+#
+# def playball(team_1_name,team_1,team_2_name,team_2):
+#     print("%s VS %s의 시합을 시작합니다." % (team_1_name, team_2_name))
+#
+
 #
 # def baseball():
 #     game_status = ["anta","strike","ball","out"]
