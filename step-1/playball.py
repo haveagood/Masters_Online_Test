f5dc5d44596ad8b9e5adeb6562cd9598adc6a7e6
haveagood <haveagood@naver.com>
2019-12-01 14:11:54 +0900

    안타, 스트라이크, 볼, 아웃을 각 별개의 함수로 쪼개어 문제를 해결하였습니다.
    추가적으로 안타, 아웃의 경우에는 다음 타자가 타석에 입장하는 경우가 존재하기 때문에 다음 타자가 입장하는 함수도 별도로 만들어서 사용하였습니다.
    
    또한, 아웃의 경우에는 3아웃이 될 경우, 다음 타자가 타석에 입장하는것이 아니라 이닝이 끝나고 결과값이 출력되게 하였습니다.
    
    스트라이크의 경우에는 3스트라이크 아웃이 될 경우, 아웃함수를 실행하도록 하였습니다.
    
    아웃카운트가 3이 될 경우 while루프를 빠져나오도록 하였고, 최종 안타수와 GAME OVER를 출력하도록 하였습니다.

--- step-1/playball.py
+++ step-1/playball.py
@@ -10,39 +10,76 @@ def baseball():
     game_cnt = [0,0,0,0]
     playball(game_status,game_cnt)
 
-def playball(game_status, game_cnt):
-    now_status = random.choice(game_status)
+def playball(game_status,game_cnt):
 
     while True:
+
         now_status = random.choice(game_status)
         print(" ")
+
         if now_status == "anta":
-            game_cnt[0] += 1
-            game_cnt[1], game_cnt[2] = 0, 0
-            print("안타! 다음 타자가 타석에 입장했습니다.")
+            game_cnt = anta(game_cnt)
         elif now_status == "strike":
-            game_cnt[1] += 1
-            print("스트라이크!")
-            if game_cnt[1] == 3:
-                print("아웃! 다음 타자가 타석에 입장했습니다.")
-                game_cnt[3] += 1
-                game_cnt[1], game_cnt[2] = 0, 0
-                if game_cnt[3] == 3:
-                    break
+            game_cnt = strike(game_cnt)
         elif now_status == "ball":
-            game_cnt[2] += 1
-            print("볼!")
-            if game_cnt[2] == 4:
-                game_cnt[0] += 1
-                print("포볼! 다음 타자가 타석에 입장했습니다.")
-                game_cnt[1], game_cnt[2] = 0, 0
+            game_cnt = ball(game_cnt)
         elif now_status == "out":
-            if game_cnt[3] == 3:
-                break
-            game_cnt[3] += 1
-            game_cnt[1], game_cnt[2] = 0, 0
-            print("아웃! 다음 타자가 타석에 입장했습니다.")
+            game_cnt = out(game_cnt)
 
         print("%dS %dB %dO" % (game_cnt[1], game_cnt[2], game_cnt[3]))
-    print("최종 안타수 : %d" % (game_cnt[0]))
+
+        if game_cnt[3] == 3:
+            print(" ")
+            break
+
+
+    print("최종 안타수 : %d" %(game_cnt[0]))
     print("GAME OVER")
+
+
+def next_player(game_cnt):
+    print("다음 타자가 타석에 입장했습니다.")
+    game_cnt[1], game_cnt[2] = 0, 0
+    return game_cnt
+
+
+def anta(game_cnt):
+    game_cnt[0] += 1
+    print("안타!", end = " ")
+    game_cnt = next_player(game_cnt)
+    return game_cnt
+
+
+def strike(game_cnt):
+    game_cnt[1] += 1
+    print("스트라이크!")
+    if game_cnt[1] == 3:
+        game_cnt = out(game_cnt)
+
+    return game_cnt
+
+
+def ball(game_cnt):
+    game_cnt[2] += 1
+    if game_cnt[2] == 4:
+        print("포볼!", end = " ")
+        game_cnt = next_player(game_cnt)
+    else:
+        print("볼!")
+    return game_cnt
+
+
+def out(game_cnt):
+    game_cnt[3] += 1
+    if game_cnt[3] == 3:
+        game_cnt[1],game_cnt[2] = 0,0
+        print("아웃!")
+        return game_cnt
+
+    else:
+        print("아웃!", end = " ")
+        game_cnt = next_player(game_cnt)
+        return game_cnt
+
+
+main()
